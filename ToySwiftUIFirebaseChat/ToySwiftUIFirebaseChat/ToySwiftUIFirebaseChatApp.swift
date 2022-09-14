193ce5e884f01bcbaae128c80a3e17955b736e25
Hyunjun Choi <hjchoi.pro@gmail.com>
2022-09-14 17:45:53 -0400

    setting up swiftui toy project

---
+++ ToySwiftUIFirebaseChat/ToySwiftUIFirebaseChat/ToySwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToySwiftUIFirebaseChatApp.swift
+//  ToySwiftUIFirebaseChat
+//
+//  Created by HYUNJUN CHOI on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ToySwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
